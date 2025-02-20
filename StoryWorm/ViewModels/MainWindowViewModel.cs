9fd1e2f8d7763057604201235dc9dea330464faa
Aaron <aaronhustudent@163.com>
2025-02-20 20:53:11 +0800

    更新了项目类型，确保跨平台顺利
    切换为Avalonia MVVM项目:)

---
+++ StoryWorm/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace StoryWorm.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "欢迎来到Avalonia！";
+}
--- StoryWorm/Views/MainWindow.axaml.cs
+++ StoryWorm/Views/MainWindow.axaml.cs
@@ -1,6 +1,6 @@
 using Avalonia.Controls;
 
-namespace StoryWorm;
+namespace StoryWorm.Views;
 
 public partial class MainWindow : Window
 {
